2059e8c9844d727d64a56691a0fa535879ddaabe
Martin Robles <robles.lprb@gmail.com>
2023-12-26 17:49:17 -0600

    Create movie mapper

---
+++ lib/data/mappers/movie_mapper.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/data/models/moviedb/movie_moviedb.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB movieDB) => Movie(
+      adult: movieDB.adult,
+      backdropPath: movieDB.backdropPath != ''
+          ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
+          : 'https://media.geeksforgeeks.org/wp-content/uploads/20230802153215/Error-404.png',
+      genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+      id: movieDB.id,
+      originalLanguage: movieDB.originalLanguage,
+      originalTitle: movieDB.originalTitle,
+      overview: movieDB.overview,
+      popularity: movieDB.popularity,
+      posterPath: movieDB.posterPath != ''
+          ? 'https: //image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
+          : 'no-poster',
+      releaseDate: movieDB.releaseDate,
+      title: movieDB.title,
+      video: movieDB.video,
+      voteAverage: movieDB.voteAverage,
+      voteCount: movieDB.voteCount);
+}
