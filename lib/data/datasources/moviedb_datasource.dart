1d9d64ca255a1f7581859a1001c95096fd7b643f
Martin Robles <robles.lprb@gmail.com>
2023-12-26 17:55:14 -0600

    Implement movie mapper

--- lib/data/datasources/moviedb_datasource.dart
+++ lib/data/datasources/moviedb_datasource.dart
@@ -1,4 +1,6 @@
 import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/data/mappers/movie_mapper.dart';
+import 'package:cinemapedia/data/models/moviedb/moviedb_response.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:dio/dio.dart';
@@ -12,7 +14,11 @@ class MovieDBDatasource extends MoviesDatasource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('movie/now_playing');
-    final List<Movie> movies = [];
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = movieDBResponse.results
+        .where((element) => element.posterPath != 'no-poster')
+        .map((movie) => MovieMapper.movieDBToEntity(movie))
+        .toList();
 
     return movies;
   }
