4068fa9a58aa5efd69fed9113f605fee03fa419c
Martin Robles <robles.lprb@gmail.com>
2023-12-26 17:28:37 -0600

    Add baseUrl

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -2,4 +2,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
   static String MOVIEDB_KEY = dotenv.env['THE_MOVIEDB_KEY'] ?? 'NO API KEY';
+  static String BASE_URL = "https://api.themoviedb.org/3/";
 }
