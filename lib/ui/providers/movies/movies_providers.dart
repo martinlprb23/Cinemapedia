8c8541324a293b19c2162bd4129698477a031c66
Martin Robles <mlopez@realmetrics.io>
2023-12-27 15:51:14 -0600

    Add validation isLoading

--- lib/ui/providers/movies/movies_providers.dart
+++ lib/ui/providers/movies/movies_providers.dart
@@ -1,3 +1,5 @@
+import 'dart:isolate';
+
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/ui/providers/movies/movies_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,13 +14,19 @@ typedef MovieCallback = Future<List<Movie>> Function({int page});
 
 class MoviesNotifier extends StateNotifier<List<Movie>> {
   int currentPage = 0;
+  bool isLoading = false;
   MovieCallback fetchMoreMovies;
 
   MoviesNotifier({required this.fetchMoreMovies}) : super([]);
 
   Future<void> loadNextPage() async {
+    if (isLoading) return;
+
+    isLoading = true;
     currentPage++;
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
     state = [...state, ...movies];
+    await Future.delayed(const Duration(milliseconds: 300));
+    isLoading = false;
   }
 }
