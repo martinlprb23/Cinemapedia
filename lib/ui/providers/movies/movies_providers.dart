d11bb994fcbf099171bfc1c963bee38e72777248
Martin Robles <mlopez@realmetrics.io>
2023-12-27 16:39:39 -0600

    Remove import

--- lib/ui/providers/movies/movies_providers.dart
+++ lib/ui/providers/movies/movies_providers.dart
@@ -1,5 +1,3 @@
-import 'dart:isolate';
-
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/ui/providers/movies/movies_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
