538723c3c7b87535269981d0e73b8306512a6fc0
Martin Robles <mlopez@realmetrics.io>
2023-12-28 13:52:22 -0600

    Add movie details mapper

--- lib/data/mappers/movie_mapper.dart
+++ lib/data/mappers/movie_mapper.dart
@@ -1,6 +1,8 @@
 import 'package:cinemapedia/data/models/moviedb/movie_moviedb.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
+import '../models/movie_details.dart';
+
 class MovieMapper {
   static Movie movieDBToEntity(MovieMovieDB movieDB) => Movie(
       adult: movieDB.adult,
@@ -21,4 +23,24 @@ class MovieMapper {
       video: movieDB.video,
       voteAverage: movieDB.voteAverage,
       voteCount: movieDB.voteCount);
+
+  static Movie movieDetailsToEntity(MovieDetails moviedb) => Movie(
+      adult: moviedb.adult,
+      backdropPath: (moviedb.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+          : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
+      genreIds: moviedb.genres.map((e) => e.name).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: (moviedb.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+          : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
+      releaseDate: moviedb.releaseDate,
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount);
 }
