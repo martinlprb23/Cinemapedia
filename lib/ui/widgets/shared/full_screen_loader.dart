a6416785e5f547b8ebd6b1dd24958d8291eeb913
Martin Robles <mlopez@realmetrics.io>
2023-12-28 11:43:38 -0600

    Add full scren loader

---
+++ lib/ui/widgets/shared/full_screen_loader.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class FullScreeLoader extends StatelessWidget {
+  const FullScreeLoader({super.key});
+
+  Stream<String> getLoadingMessages() {
+    final messages = <String>[
+      'Loading movies',
+      'Buying popcorn',
+      'Buying ice cream',
+      'Almost ready',
+      'This is taking longer than expected :('
+    ];
+
+    return Stream.periodic(
+            const Duration(microseconds: 1200), (step) => messages[step])
+        .take(messages.length);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Please wait'),
+          const SizedBox(height: 16),
+          const CircularProgressIndicator(),
+          const SizedBox(height: 16),
+          StreamBuilder(
+            stream: getLoadingMessages(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const Text('Loading...');
+              return Text(snapshot.data!);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
