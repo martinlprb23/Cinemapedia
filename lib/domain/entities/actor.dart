85ee49a9c0f4cf025d3f022901cf3a31290e1ad8
Martin Robles <mlopez@realmetrics.io>
2023-12-29 10:28:24 -0600

    Add class actor

---
+++ lib/domain/entities/actor.dart
@@ -0,0 +1,12 @@
+class Actor {
+  final int id;
+  final String name;
+  final String image;
+  final String? character;
+
+  Actor(
+      {required this.id,
+      required this.name,
+      required this.image,
+      required this.character});
+}
