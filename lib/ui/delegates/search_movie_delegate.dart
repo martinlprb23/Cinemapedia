2d9907aaf3e239eb507bde6b34a857cfd1c5316d
Martin Robles <mlopez@realmetrics.io>
2024-01-02 10:01:12 -0600

    Add search movie screen

---
+++ lib/ui/delegates/search_movie_delegate.dart
@@ -0,0 +1,119 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
+import 'package:flutter/material.dart';
+
+import '../../domain/entities/movie.dart';
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({required this.searchMovies});
+
+  @override
+  String get searchFieldLabel => 'Search Movie';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      //if (query.isNotEmpty)
+      FadeIn(
+          animate: query.isNotEmpty,
+          child: IconButton(
+              onPressed: () => query = '', icon: const Icon(Icons.clear)))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: const Icon(Icons.arrow_back_ios));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text('buildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder(
+        future: searchMovies(query),
+        initialData: const [],
+        builder: (context, snapshot) {
+          final movies = snapshot.data ?? [];
+          return ListView.builder(
+            itemCount: movies.length,
+            itemBuilder: (context, index) => _MovieItem(movie: movies[index]),
+          );
+        });
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        children: [
+          //IMAGE
+          SizedBox(
+            width: size.width * 0.3,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                  movie.posterPath != 'no-poster'
+                      ? movie.posterPath
+                      : 'https://images.unsplash.com/photo-1594909122845-11baa439b7bf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) =>
+                      FadeIn(child: child)),
+            ),
+          ),
+          const SizedBox(width: 16),
+          //Description
+          SizedBox(
+            width: size.width * 0.5,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  movie.title,
+                  style: textStyle.titleMedium,
+                ),
+                const SizedBox(height: 8),
+                (movie.overview.length > 100)
+                    ? Text(
+                        '${movie.overview.substring(0, 100)}...',
+                      )
+                    : Text(movie.overview),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star_half_outlined,
+                      color: Colors.yellow.shade800,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(HumanFormats.number(movie.voteAverage, 1),
+                        style: textStyle.bodyMedium!
+                            .copyWith(color: Colors.yellow.shade900))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
