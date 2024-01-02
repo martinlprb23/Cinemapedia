7aa41c856febed9d20c5d95ea8b4076f7bf8260f
Martin Robles <mlopez@realmetrics.io>
2024-01-02 10:00:30 -0600

    Add decimal places

--- lib/config/helpers/human_formats.dart
+++ lib/config/helpers/human_formats.dart
@@ -1,10 +1,10 @@
 import 'package:intl/intl.dart';
 
 class HumanFormats {
-  static String number(double number) {
-    final formattedNumber =
-        NumberFormat.compactCurrency(decimalDigits: 0, symbol: '', locale: 'en')
-            .format(number);
+  static String number(double number, [int decimalPlaces = 0]) {
+    final formattedNumber = NumberFormat.compactCurrency(
+            decimalDigits: decimalPlaces, symbol: '', locale: 'en')
+        .format(number);
 
     return formattedNumber;
   }
