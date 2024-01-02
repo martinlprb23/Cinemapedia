30a28ecbc366efab416c39d35f96f9b4d46479ea
Martin Robles <mlopez@realmetrics.io>
2024-01-02 12:47:35 -0600

    Go Router oficial indications

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,7 +3,29 @@ import 'package:cinemapedia/ui/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+            path: '/',
+            builder: (context, state) => const HomeView(),
+            routes: [
+              GoRoute(
+                  path: 'movies/:id',
+                  name: MovieScreen.name,
+                  builder: (context, state) {
+                    final movieId = state.pathParameters['id'] ?? 'no-id';
+                    return MovieScreen(movieId: movieId);
+                  })
+            ]),
+        GoRoute(
+            path: '/favorites',
+            builder: (context, state) => const FavoritesView())
+      ])
+
+  /*GoRoute(
       path: '/',
       name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(childView: HomeView()),
@@ -16,5 +38,5 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
             return MovieScreen(movieId: movieId);
           },
         )
-      ]),
+      ]),*/
 ]);
--- lib/ui/screens/movies/home_screen.dart
+++ lib/ui/screens/movies/home_screen.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: childView,
-      bottomNavigationBar: const CustomBottomNav(),
+      bottomNavigationBar: const CustomBottomNavigation(),
     );
   }
 }
