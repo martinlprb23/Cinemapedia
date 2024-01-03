744c927a7712dbbcb48161da9ea07191f886d480
Martin Robles <mlopez@realmetrics.io>
2024-01-03 13:01:54 -0600

    Add export popular movies

--- lib/ui/views/views.dart
+++ lib/ui/views/views.dart
@@ -1,2 +1,3 @@
 export 'package:cinemapedia/ui/views/movies/home_view.dart';
+export 'package:cinemapedia/ui/views/movies/popular_view.dart';
 export 'package:cinemapedia/ui/views/movies/favorites_view.dart';
