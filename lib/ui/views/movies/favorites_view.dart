55bdbf8b9992be5b967e322b3319b59fe6d0fc31
Martin Robles <mlopez@realmetrics.io>
2024-01-03 11:57:40 -0600

    Add empty favorite movies

--- lib/ui/views/movies/favorites_view.dart
+++ lib/ui/views/movies/favorites_view.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia/ui/providers/providers.dart';
 import 'package:cinemapedia/ui/widgets/movies/movie_masonry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoriteView extends ConsumerStatefulWidget {
   const FavoriteView({super.key});
@@ -33,6 +34,27 @@ class FavoriteViewState extends ConsumerState<FavoriteView> {
   @override
   Widget build(BuildContext context) {
     final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+    if (favoriteMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary),
+            Text('Ohhh no!!',
+                style: TextStyle(fontSize: 30, color: colors.primary)),
+            const Text('No tienes películas favoritas',
+                style: TextStyle(fontSize: 20, color: Colors.black45)),
+            const SizedBox(height: 20),
+            FilledButton.tonal(
+                onPressed: () => context.go('/home/0'),
+                child: const Text('Empieza a buscar'))
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
         appBar: AppBar(title: const Text('Favorites')),
