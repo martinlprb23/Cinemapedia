2311a4a91a68ca40a9c04a85e4ec2b8815d23fdd
Martin Robles <mlopez@realmetrics.io>
2024-01-03 13:18:45 -0600

    Update design and add loader

--- lib/ui/widgets/movies/movie_porter_link.dart
+++ lib/ui/widgets/movies/movie_porter_link.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
@@ -9,13 +11,21 @@ class MoviePosterLink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final random = Random();
+
     return FadeInUp(
+      from: random.nextInt(100) + 80,
+      delay: Duration(milliseconds: random.nextInt(450) + 0),
       child: GestureDetector(
         onTap: () => context.push('/home/0/movies/${movie.id}'),
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Image.network(movie.posterPath),
-        ),
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+                height: 180,
+                fit: BoxFit.cover,
+                image: NetworkImage(movie.posterPath),
+                placeholder:
+                    const AssetImage('assets/loaders/bottle-loader.gif'))),
       ),
     );
   }
