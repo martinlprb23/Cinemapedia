d747caec531068c2795aeaaefde6609c99b2bfe3
Martin Robles <mlopez@realmetrics.io>
2024-01-03 13:26:48 -0600

    Remove unnecesary import

--- lib/ui/widgets/movies/movies_slideshow.dart
+++ lib/ui/widgets/movies/movies_slideshow.dart
@@ -1,4 +1,3 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
