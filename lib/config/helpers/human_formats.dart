63e76ae8c1b1a450cb22c092ce5db11c66743d7d
Martin Robles <mlopez@realmetrics.io>
2024-01-03 15:21:16 -0600

    TODO, fix date format

--- lib/config/helpers/human_formats.dart
+++ lib/config/helpers/human_formats.dart
@@ -8,4 +8,10 @@ class HumanFormats {
 
     return formattedNumber;
   }
+
+  static String shortDate(DateTime date) {
+    //TODO fix bug: final format = DateFormat.yMMMEd('es');
+    //return format.format(date);
+    return date.toLocal().toString();
+  }
 }
