1915af0b63b93e579f7111fdf8319f528627d0b2
Martin Robles <mlopez@realmetrics.io>
2024-01-03 10:36:40 -0600

    Add favorite movies provider

---
+++ lib/ui/providers/storage/favorites_movies_provider.dart
@@ -0,0 +1,34 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/local_sorage_repository.dart';
+import 'package:cinemapedia/ui/providers/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final favoriteMoviesProvider =
+    StateNotifierProvider<SotrageMoviesNotifier, Map<int, Movie>>((ref) {
+  final localStorageRepository = ref.watch(localStorageRepoProvider);
+  return SotrageMoviesNotifier(localStorageRepository: localStorageRepository);
+});
+/*
+  {
+    1234, Movie,
+    1224, Movie
+  }
+*/
+
+class SotrageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
+  int page = 0;
+  final LocalStorageRepository localStorageRepository;
+  SotrageMoviesNotifier({required this.localStorageRepository}) : super({});
+
+  Future<void> loadNextPage() async {
+    final movies = await localStorageRepository.loadMovies(offset: page * 10);
+    page++;
+
+    final tempMoviesMap = <int, Movie>{};
+    for (final movie in movies) {
+      tempMoviesMap[movie.id] = movie;
+    }
+
+    state = {...state, ...tempMoviesMap};
+  }
+}
