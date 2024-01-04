cd078bb57fdaeef5e3ef8388467739165b6e7e57
Martin Robles <mlopez@realmetrics.io>
2024-01-04 09:23:05 -0600

    Add mxing to save the state

--- lib/ui/screens/movies/home_screen.dart
+++ lib/ui/screens/movies/home_screen.dart
@@ -3,18 +3,56 @@ import 'package:flutter/material.dart';
 
 import '../../views/views.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const name = 'home-screen';
   final int pageIndex;
 
   const HomeScreen({super.key, required this.pageIndex});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+//This mixin in necesary to save the state of the page view
+class _HomeScreenState extends State<HomeScreen>
+    with AutomaticKeepAliveClientMixin {
+  late PageController pageController;
   final viewRoutes = const <Widget>[HomeView(), PopularView(), FavoriteView()];
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: IndexedStack(index: pageIndex, children: viewRoutes),
-        bottomNavigationBar: CustomBottomNav(currentIndex: pageIndex));
+  void initState() {
+    super.initState();
+    pageController = PageController(keepPage: true);
   }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+
+    if (pageController.hasClients) {
+      pageController.animateToPage(
+        widget.pageIndex,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOut,
+      );
+    }
+
+    return Scaffold(
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: viewRoutes,
+      ),
+      bottomNavigationBar: CustomBottomNav(currentIndex: widget.pageIndex),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
 }
